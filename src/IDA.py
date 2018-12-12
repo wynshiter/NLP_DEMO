87b1e25f5803adaafd57620d7243a04c732c80df
wynshiter <shiter@live.cn>
2018-12-12 22:52:50 +0800

    Update the project directory structure, already wordcloud

---
+++ src/IDA.py
@@ -0,0 +1,79 @@
+# encoding: utf-8
+'''
+@author: season
+@contact: shiter@live.cn
+
+@file: IDA.py
+@time: 2018/12/9 23:33
+@desc:
+'''
+
+import file_operator
+
+import  jieba
+import pandas as pd
+file_path = u'''blog'''
+file_list = file_operator.all_file_name_with_extension(file_path,'.txt')
+
+
+data = pd.dataframe(file_list,columns=['file_name'])
+
+#分词，暂时没有去停用词
+def chinese_word_cut(file_name):
+    mytext = ''
+    with open(file_name,'r',encoding='utf-8') as f:
+        mytext = f.read()
+    return " ".join(jieba.cut(mytext))
+
+
+#没有简介的医院替换句号
+data['summary'] = data['summary'].fillna('.').astype('str')
+#新增分词后的字段
+data["content_cutted"] =data['summary'].apply(chinese_word_cut)
+
+data_head = data.content_cutted.head()
+
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+#从文本中提取1000个最重要的特征关键词
+n_features = 1000
+tf_vectorizer = CountVectorizer(strip_accents = 'unicode',
+                                max_features=n_features,
+                                stop_words='english',
+                                max_df = 0.5,
+                                min_df = 10)
+tf = tf_vectorizer.fit_transform(data.content_cutted)
+
+from sklearn.decomposition import LatentDirichletAllocation
+
+n_topics = 5
+lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=50,
+                                learning_method='online',
+                                learning_offset=50.,
+                                random_state=0)
+
+
+lda.fit(tf)
+
+#
+def print_top_words(model, feature_names, n_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        print("Topic #%d:" % topic_idx)
+        print(" ".join([feature_names[i]
+                        for i in topic.argsort()[:-n_top_words - 1:-1]]))
+    print()
+
+
+n_top_words = 20
+
+tf_feature_names = tf_vectorizer.get_feature_names()
+print_top_words(lda, tf_feature_names, n_top_words)
+
+import pyLDAvis
+import pyLDAvis.sklearn
+pyLDAvis.enable_notebook()
+pyLDAvis.sklearn.prepare(lda, tf, tf_vectorizer)
+
+
+data = pyLDAvis.sklearn.prepare(lda, tf, tf_vectorizer)
+pyLDAvis.show(data)
+#pyLDAvis.display()
