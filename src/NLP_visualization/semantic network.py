71496f5e3ee3c0ae1d37db5cc16314dfd80356f5
wynshiter <shiter@live.cn>
2020-09-22 13:38:36 +0800

    20200922 add code about visualization of semantic network

--- src/NLP_visualization/semantic network.py
+++ src/NLP_visualization/semantic network.py
@@ -21,8 +21,10 @@ import pandas as pd
 import networkx as nx  #复杂网络分析库
 import matplotlib.pyplot as plt
 
-num=40
+# 图中节点个数
+num=20
 G=nx.Graph()
+
 plt.figure(figsize=(20,14))
 plt.rcParams['axes.unicode_minus'] = False    # 用来正常显示负号
 plt.rcParams['font.sans-serif'] = ['SimHei']   # 用来正常显示中文标签
@@ -45,12 +47,18 @@ stop_words = [x.replace('\n','') for x in remove_words ]
 # for x in remove_words:
 #     x = x.replace('\n', '')
 #     stop_words.append(x)
+stop_words.append('''\n''')
+stop_words.append('')
+stop_words.append(u'\xa0')
+stop_words.append(' ')
 
+stop_words = set(stop_words)
 print(stop_words)
 
 for word in seg_list_exact: # 循环读出每个分词
-    if word not in stop_words: # 如果不在去除词库中
+    if word not in stop_words : # 如果不在去除词库中
         object_list.append(word) # 分词追加到列表
+
 
 # 词频统计
 word_counts = collections.Counter(object_list) # 对分词做词频统计
@@ -69,39 +77,46 @@ for i in range(len(string_data)):
     if string_data[i] == '\n':  #根据换行符读取一段文字
         seg_list_exact = jieba.cut(string_data[k:i], cut_all = False) # 精确模式分词
         object_list2 = []
+
         for words in seg_list_exact: # 循环读出每个分词
             if words not in stop_words: # 如果不在去除词库中
                 object_list2.append(words) # 分词追加到列表
 
-word_counts2 = collections.Counter(object_list2)
-word_counts_top2 = word_counts2.most_common(num) # 获取该段最高频的词
-word2 = pd.DataFrame(word_counts_top2)
-word2_T = pd.DataFrame(word2.values.T,columns=word2.iloc[:,0])
-relation = list(0 for x in range(num))
+        if len(object_list2)==0:## 跳过空段落
+            continue
 
 
+        word_counts2 = collections.Counter(object_list2)
+        word_counts_top2 = word_counts2.most_common(num) # 获取该段最高频的词
 
-# 查看该段最高频的词是否在总的最高频的词列表中
-for j in range(num):
-    for p in range(len(word2)):
-        if word.iloc[j,0] == word2.iloc[p,0]:
-            relation[j] = 1
-            break
-        #对于同段落内出现的最高频词，根据其出现次数加到语义关联矩阵的相应位置
+        word2 = pd.DataFrame(word_counts_top2)
+        word2_T = pd.DataFrame(word2.values.T,columns=word2.iloc[:,0])
+        relation = list(0 for x in range(num))
 
-for j in range(num):
-    if relation[j] == 1:
-        for q in range(num):
-            if relation[q] == 1:
-                net.iloc[j, q] = net.iloc[j, q] + word2_T.loc[1, word_T.iloc[0, q]]
-    k = i + 1
+
+    # 查看该段最高频的词是否在总的最高频的词列表中
+        for j in range(num):
+            for p in range(len(word2)):
+                if word.iloc[j,0] == word2.iloc[p,0]:
+                    relation[j] = 1
+                    break
+            #对于同段落内出现的最高频词，根据其出现次数加到语义关联矩阵的相应位置
+
+        for j in range(num):
+            if relation[j] == 1:
+                for q in range(num):
+                    if relation[q] == 1:
+                        net.iloc[j, q] = net.iloc[j, q] + word2_T.loc[1, word_T.iloc[0, q]]
+
+        k = i + 1
 
             # 处理最后一段内容，完成语义关联矩阵的构建
 
 print('关联矩阵构造完成--------------')
 
 n = len(word)
-        # 边的起点，终点，权重
+
+# 边的起点，终点，权重
 
 for i in range(n):
     for j in range(i, n):
@@ -110,13 +125,22 @@ print(G.edges())
 
 print('开始进行绘制--------------')
 
+my_width = [float(v['weight'] / 10) for (r, c, v) in G.edges(data=True)]
+my_node_size=[float(net.iloc[i, i] * 6) for i in np.arange(num)]
+
+print(my_node_size)
+print(len(my_width))
+print(len(my_node_size))
+print(len(G.nodes()))
+
 nx.draw_networkx(G,
                     pos=nx.spring_layout(G),
                     # 根据权重的大小，设置线的粗细
-                    width=[float(v['weight'] / 10) for (r, c, v) in G.edges(data=True)],
+                    width=my_width,
                     edge_color='orange',
                     # 根据词出现的次数，设置点的大小
-                    node_size=[float(net.iloc[i, i] * 6) for i in np.arange(20)],node_color='white'
+                    node_size=my_node_size,
+                    node_color='white'
                     )
 #plt.axis('off')
 #plt.title('助攻表现（常规赛）',fontstyle='oblique')
